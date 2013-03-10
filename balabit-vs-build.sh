94ba0ccd837e83f478d316b37f2d5d7346937cf2
Juhasz Viktor <jviktor@balabit.hu>
2013-03-10 13:26:21 +0100

    libxml2: make possible to compile libxml2 in zbs
    Signed-off-by: Juhasz Viktor <jviktor@balabit.hu>

---
+++ balabit-vs-build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+cmd=$1
+shift
+case "$cmd" in
+  get-version)
+    echo 0.36-sng50ref1
+    ;;
+  build-exclude-list|dist-exclude-list|prepare-dist)
+    echo "out autom4te.cache"
+    ;;
+  bootstrap)
+    sh ./autogen.sh
+    find -name libtool -o -name ltmain.sh | xargs sed -i -e "s,'file format pe-i386.*\?','file format \(pei\*-i386\(\.\*architecture: i386\)\?|pe-arm-wince|pe-x86-64\)'," -e 's,cmd \/\/c,,'
+    ;;
+  configure)
+    CFLAGS="-g -O2" LDFLAGS="-g" ./configure $@
+    ;;
+  make)
+    make $@
+    ;;
+  *)
+    echo "Unknown command: $cmd"
+    exit 1
+    ;;
+esac
+
+# vim: ts=2 sw=2 expandtab
